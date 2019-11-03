611bb141d3bac1e7ee3d5006e6c369c40ee9b323
Rocco <roccoriu@gmail.com>
2019-11-03 19:47:05 +0100

    base website working

---
+++ scripts.js
@@ -0,0 +1,6 @@
+$('document').ready(function () {
+   console.log('====================================');
+   console.log('this is not working');
+   console.log('===================================='); 
+});
+
