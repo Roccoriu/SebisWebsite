3f7d956861637165cb8e859bfd2dc9a4b91f2a80
Rocco <roccoriu@gmail.com>
2019-11-03 21:51:05 +0100

    about page finished

--- scripts.js
+++ scripts.js
@@ -1,6 +1,4 @@
 $('document').ready(function () {
-   console.log('====================================');
-   console.log('this is not working');
-   console.log('===================================='); 
+   console.log('this is working');
 });
 
